7346f7125672288258bce3b8ad34bd8dcfeaf513
emersonsdnz <emersonsdnz@outlook.com>
2021-12-03 17:54:03 +1300

    img

--- Fractals.py
+++ Fractals.py
@@ -6,7 +6,13 @@ REPS = 10 #number of times newtons method is applied
 cols = ["red", "green", "blue", "yellow", "purple", "pink", "white", "black"]
 
 #ie [2,4,-3,2] will be 2x^3 + 4x^2 - 3x + 2
-p = [6,5,8,3,2,1]
+p = []
+
+print("Enter nums, type done when done: ")
+usr_in = input("")
+while (not usr_in.endswith("done") and len(p)<9):
+    p.append(int(usr_in))
+    usr_in = input("")
 
 def f(x): #returns value of polynomial defined by array p at x
     ans = 0
@@ -39,7 +45,9 @@ def get_col(x): #gets the color of the image drawn based on the root the complex
             min_dist = d
     return cols[root_num]
 
-size = 1000
+
+size = int(input("Enter size of output:\n"))
+max_coord = int(input("Enter the boundary for the fractal: "))
 
 img = Image.new(mode = "RGB", size = (size, size))
 draw = ImageDraw.Draw(img)
@@ -48,7 +56,8 @@ print("Generating - this might take a while!")
 
 for x in range(size): #This is where the biz happens
     for y in range(size):
-        draw.point((x,y), fill=get_col(newtons((x-(size/2))/250+0.00001, (y-(size/2))/250)))
+        d=(size/2)/max_coord
+        draw.point((x,y), fill=get_col(newtons((x-(size/2))/d+0.00001, (y-(size/2))/d)))
 
 img.show()
 img.save("frac_" + str(random.randrange(0,1000000)) + ".png")
