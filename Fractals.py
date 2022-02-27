b95f5e0f2e20d73b8c938c2f9e1afb38d109cfe1
emersonsdnz <emersonsdnz@outlook.com>
2022-02-27 19:02:14 +1300

    stuff

--- Fractals.py
+++ Fractals.py
@@ -26,9 +26,9 @@ def df(x): #returns gradient of polynomial defined by array p at x
         ans += p[a] * (len(p)-a-1) * x**(len(p)-a-2)
     return ans
 
-def newtons(x, y): #applies newtons method to find roots
+def newtons(x, y, reps): #applies newtons method to find roots
     comp = complex(x, y)
-    for i in range(REPS):
+    for i in range(reps):
         comp = comp - f(comp)/df(comp)
     return comp
 
@@ -47,19 +47,27 @@ def get_col(x): #gets the color of the image drawn based on the root the complex
 
 
 size = int(input("Enter size of output:\n"))
-max_coord = int(input("Enter the boundary for the fractal: "))
-
-img = Image.new(mode = "RGB", size = (size, size))
-draw = ImageDraw.Draw(img)
+max_coord = float(input("Enter the boundary for the fractal: "))
+n_frames = int(input("Enter num frames:\n"))
 
 print("Generating - this might take a while!")
 
-for x in range(size): #This is where the biz happens
-    for y in range(size):
-        d=(size/2)/max_coord
-        draw.point((x,y), fill=get_col(newtons((x-(size/2))/d+0.00001, (y-(size/2))/d)))
+images = []
 
-img.show()
-img.save("frac_" + str(random.randrange(0,1000000)) + ".png")
+for t in range(n_frames):
+    img = Image.new(mode = "RGB", size = (size, size))
+    draw = ImageDraw.Draw(img)
+    d = (size/2)/(max_coord*np.exp(-t/17))
+    for x in range(size): #This is where the biz happens
+        for y in range(size):
+            draw.point((x,y), fill=get_col(newtons((x-(size/2))/d+0.00001, (y-(size/2))/d, int(REPS*np.exp(t/30)))))
+    images.append(img)
+    img.save("frames/f" + str(t) + ".png")
+    print("Frame " + str(t) + " drawn with " + str(int(REPS*np.exp(t/30))) + " reps")
+
+images[0].save("frac_zoom_" + str(random.randrange(0,1000000)) + ".gif", save_all=True, append_images = images[1:], optimize = False, duration = 50)
+
+#img.show()
+#img.save("frac_" + str(random.randrange(0,1000000)) + ".png")
 
 print("done!")
